6f3ca92c7cf6577e9a43e26cfdd853c66e5db5e3
Mike Slinn <mslinn@mslinn.com>
2025-09-29 12:07:13 -0400

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require_relative '../lib/jekyll_img'
 
 RSpec.configure do |config|
   config.filter_run_when_matching focus: true
-  config.order = 'random'
+  # config.order = 'random'
 
   # See https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures
   config.example_status_persistence_file_path = 'spec/status_persistence.txt'
